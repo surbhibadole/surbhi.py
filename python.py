aff266c028a9dc8df75d6d3f159e6958d41a3b7f
surbhi <surbhi.mujmer@gmail.com>
2023-07-05 02:33:21 -0700

    this is my complete code

--- python.py
+++ python.py
@@ -5,3 +5,19 @@ def subtract(x, y):
     return x - y
 def multiply(x, y):
     return x * y
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero."
+    print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice in ['1', '2', '3', '4']:
+        num1 = float(input("Enter the first"))
